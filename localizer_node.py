31377889b8ee55acd939885a403b0e4d6bde26a6
Manash Pratim Das <mpdmanash@gmail.com>
2022-04-25 13:11:50 +0100

    major changes

--- localizer_node.py
+++ localizer_node.py
@@ -1,25 +1,27 @@
 import random
+import cv2
 import socket
 from ctypes import *
 import numpy as np
 import binascii
 import rospy
+import tf
 from geometry_msgs.msg import Pose2D
+from scipy.spatial.transform import Rotation
 
 class AprilTag:
-    def __init__(self, id, center, p1, p2, p3, p4):
+    def __init__(self, id, center, quat):
         self.id = id
         self.center = center
-        self.p1 = p1
-        self.p2 = p2
-        self.p3 = p3
-        self.p4 = p4
+        self.quat = quat
         self.angle = 0.0
 
-    def computeAngle(self):
-        D = self.p3-self.p2
-        self.angle = -np.arctan2(-D[1],D[0])*180.0/np.pi
-        #self.angle = np.arccos(D[0]/np.linalg.norm(D))*180.0/np.pi
+    def getWPose2D(self,wR,wT):
+        wC = np.dot(wR, self.center.reshape((3,1))) + wT
+        wR = Rotation.from_dcm(np.dot(wR, Rotation.from_quat(self.quat).as_dcm())).as_euler('zyx', degrees=True)
+        print(wR)
+        # return wC[0], wC[1], wR[2]
+        
 
 
 
@@ -56,10 +58,15 @@ g_t = np.zeros((3,))
 #                   1: (0.6,0.6,0.1)}
 
 ud = 0.6
-g_associations = {4: np.array([  0.0,0.0, 0.169/ud  ])*ud,
-                  2: np.array([  2.0,0.0, 0.189/ud  ])*ud,
-                  5: np.array([  2.0,-2.0, 0.217/ud  ])*ud,
-                  1: np.array([  1.0,-2.0, 0.185/ud  ])*ud}
+g_associations = {4: np.array([  3.0,-2.0, 0.169/ud  ])*ud,
+                  2: np.array([  0.0,0.0, 0.189/ud  ])*ud,
+                  5: np.array([  3.0,-1.0, 0.217/ud  ])*ud,
+                  1: np.array([  2.0,0.0, 0.185/ud  ])*ud}
+g_square_half_lengths = {4: 0.168/2.0,
+                         2: 0.168/2.0,
+                         5: 0.168/2.0,
+                         1: 0.168/2.0,
+                         3: 0.168/2.0}
 
 g_pub = rospy.Publisher('atagpose', Pose2D, queue_size=1)
 
@@ -79,7 +86,7 @@ def umeyama(P, Q):
     varP = np.var(P, axis=0).sum()
     c = 1/varP * np.sum(S) # scale factor
     t = Q.mean(axis=0) - P.mean(axis=0).dot(c*R)
-    return c, R, t
+    return c, R.transpose(), t
 
 
 def h2f(s):
@@ -105,6 +112,27 @@ def get3Dcoordinates(D,K,s,R,t,x,y):
 def transformNscale3D(s, R, t, coord):
     return (np.matmul(R, (s*coord.transpose()))).transpose() + t
 
+def get3DforMarker(marker_id, p0, p1, p2, p3, c):
+    model_points = [
+                        [ -g_square_half_lengths[marker_id],  g_square_half_lengths[marker_id], 0 ],
+                        [  g_square_half_lengths[marker_id],  g_square_half_lengths[marker_id], 0 ],
+                        [  g_square_half_lengths[marker_id], -g_square_half_lengths[marker_id], 0 ],
+                        [ -g_square_half_lengths[marker_id], -g_square_half_lengths[marker_id], 0 ],
+                        [ 0.0, 0.0, 0 ]
+                   ]
+    image_points = [p0,p1,p2,p3,c]
+    (_, rvec, tvec) = cv2.solvePnP(np.array(model_points), np.array(image_points), g_K, np.array(g_D))
+    rtheta = np.linalg.norm(rvec)
+    rdir = (rvec/rtheta)*np.sin(rtheta)
+    rquat = (rdir[0,0], rdir[1,0], rdir[2,0], np.cos(rtheta))
+    br = tf.TransformBroadcaster()
+    br.sendTransform((tvec[0,0], tvec[1,0], tvec[2,0]),
+                     rquat,
+                     rospy.Time.now(),
+                     'tag:'+str(marker_id),
+                     "camera")
+    return rvec[:,0], tvec[:,0], rquat
+
 
 def process_apriltag_msg(hex_string):
     time = int(read_part(hex_string, p_time), 16)
@@ -129,18 +157,8 @@ def process_apriltag_msg(hex_string):
             p40 = h2f(read_part(tag_string, p_p40))
             p41 = h2f(read_part(tag_string, p_p41))
 
-            p13d = get3Dcoordinates(g_D,g_K,1.0,None,None,p10,p11)
-            p23d = get3Dcoordinates(g_D,g_K,1.0,None,None,p20,p21)
-            p33d = get3Dcoordinates(g_D,g_K,1.0,None,None,p30,p31)
-            p43d = get3Dcoordinates(g_D,g_K,1.0,None,None,p40,p41)
-
-            c3d = get3Dcoordinates(g_D,g_K,1.0,None,None,c0,c1)
-            # a1 = np.linalg.norm(p23d-p13d)
-            # a2 = np.linalg.norm(p33d-p23d)
-            # a3 = np.linalg.norm(p43d-p33d)
-            # a4 = np.linalg.norm(p13d-p43d)
-
-            atags.append(AprilTag(tag_id,c3d,p13d,p23d,p33d,p43d))
+            rvec, tvec, quat = get3DforMarker(tag_id, [p10,p11], [p20,p21], [p30,p31], [p40,p41], [c0,c1])
+            atags.append(AprilTag(tag_id,tvec,quat))
     return atags
 
 def calibrate_AprilTags(atags):
@@ -156,20 +174,22 @@ def calibrate_AprilTags(atags):
     if P.shape[0] >=3 :
         c,R,t = umeyama(P,Q)
         g_s = c
-        g_R = R
+        g_R = c*R
         g_t = t
+        q = Rotation.from_dcm(c*R).as_quat()
+        br = tf.TransformBroadcaster()
+        br.sendTransform((t[0], t[1], t[2]),
+                        (q[0], q[1], q[2], q[3]),
+                        rospy.Time.now(),
+                        'camera',
+                        "world")
 
 def localize_AprilTags(atags):
     for atag in atags:
         if atag.id not in g_associations and atag.id==3:
-            atag.center = transformNscale3D(g_s,g_R,g_t,atag.center)
-            atag.p1 = transformNscale3D(g_s,g_R,g_t,atag.p1)
-            atag.p2 = transformNscale3D(g_s,g_R,g_t,atag.p2)
-            atag.p3 = transformNscale3D(g_s,g_R,g_t,atag.p3)
-            atag.p4 = transformNscale3D(g_s,g_R,g_t,atag.p4)
-            atag.computeAngle()
-            msg = Pose2D(atag.center[0], atag.center[1], atag.angle)
-            g_pub.publish(msg)
+            atag.getWPose2D(g_R, g_t)
+            # msg = Pose2D(atag.center[0], atag.center[1], atag.angle)
+            # g_pub.publish(msg)
             #print atag.angle, atag.center 
 
 
@@ -178,7 +198,7 @@ def main():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     server_socket.bind(('192.168.10.119', 7709))
     rospy.init_node('ataglocalizer', anonymous=True)
-    while True:
+    while not rospy.is_shutdown():
         message, address = server_socket.recvfrom(5*112)
         byte_array = bytearray(message)
         hexadecimal_string = binascii.hexlify(byte_array)
